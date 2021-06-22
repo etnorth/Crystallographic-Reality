2060b4a8ebdb989e3b44c1f34066478fd2fc8254
CyberMancerGamer <erlend.north@hotmail.com>
2021-06-22 16:45:20 +0200

    XR Input Actions + some Unit Cell updates

--- Crystallographic Reality/Assets/Scripts/Crystal.cs
+++ Crystallographic Reality/Assets/Scripts/Crystal.cs
@@ -6,12 +6,27 @@ using System.IO; // IO: InputOutput. Used to read our input file
 
 public class Crystal : MonoBehaviour
 {
-    public GameObject parent;
-    public GameObject Atom;
+    public GameObject parent; // An object to be used as the parent of the unit cell
+    public GameObject Atom; // Atom prefab
+    public int NumOfAtoms; // Total number of atoms
+    public GameObject[] Atoms; // Array of atom objects. Initialized in Start
+    public float scaleChange = 0.1f; // A scale for making unit cell smaller/larger.
 
     // Start is called before the first frame update
     void Start()
     {
+        // Initialization
+        Read_cif();
+
+        Atoms = new GameObject[NumOfAtoms]; // Initializes list of atoms
+        for (int i = 0; i < NumOfAtoms; i++) // Loops over each atom
+        {
+            Atoms[i] = Instantiate(Atom, parent.transform, false); // Instantiate (Spawn) atom i
+            Atoms[i].transform.Translate(i, 0, 0); // Move atom i by x-direction. This will correspond to atom positions after read_cif is done
+            //Atoms[i].transform.localScale += scalechange;
+        }
+
+
         TestCreateCell();
         //Read();
     }
@@ -19,26 +34,50 @@ public class Crystal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        //Scale unit cell down
+        /*
+        if (trigger)
+        {
+            Debug.Log("Left Controller Trigger is held down");
+            parent.transform.localScale -= new Vector3(scaleChange, scaleChange, scaleChange);
+        }
+
+        //Scale unit cell up
+        if (Input.GetKey("Axis1D.SecondaryHandTrigger"))
+        {
+            Debug.Log("Right Controller Trigger is held down");
+            parent.transform.localScale += new Vector3(scaleChange, scaleChange, scaleChange);
+        }
+        */
+    }
+
+    // Read_cif is called in Start
+    void Read_cif()
+    {
+        //Reads a cif-file given by the user
 
     }
 
     // TestCreateCell is called in Start
     void TestCreateCell()
     {
-        // This function is a test and will create a unit cell
-        var atom1 = Instantiate(Atom, parent.transform, false); // Creates an atom at the parent's position
-        var atom2 = Instantiate(Atom, parent.transform, false); atom2.transform.Translate(1f, 0f, 0f); // Creates an atom and moves it from the parent's position
-        var atom3 = Instantiate(Atom, parent.transform, false); atom3.transform.Translate(0f, 1f, 0f); // Creates an atom and moves it from the parent's position
-        var atom4 = Instantiate(Atom, parent.transform, false); atom4.transform.Translate(0f, 0f, 1f); // Creates an atom and moves it from the parent's position
-        var atom5 = Instantiate(Atom, parent.transform, false); atom5.transform.Translate(0f, 1f, 1f); // Creates an atom and moves it from the parent's position
-        var atom6 = Instantiate(Atom, parent.transform, false); atom6.transform.Translate(1f, 0f, 1f); // Creates an atom and moves it from the parent's position
-        var atom7 = Instantiate(Atom, parent.transform, false); atom7.transform.Translate(1f, 1f, 0f); // Creates an atom and moves it from the parent's position
-        var atom8 = Instantiate(Atom, parent.transform, false); atom8.transform.Translate(1f, 1f, 1f); // Creates an atom and moves it from the parent's position
-        var atom9 = Instantiate(Atom, parent.transform, false); atom9.transform.Translate(0.25f, 0.25f, 0.25f); // Creates an atom and moves it from the parent's position
-        var atom10 = Instantiate(Atom, parent.transform, false); atom10.transform.Translate(0.25f, 0.75f, 0.75f); // Creates an atom and moves it from the parent's position
-        var atom11 = Instantiate(Atom, parent.transform, false); atom11.transform.Translate(0.75f, 0.25f, 0.75f); // Creates an atom and moves it from the parent's position
-        var atom12 = Instantiate(Atom, parent.transform, false); atom12.transform.Translate(0.75f, 0.75f, 0.25f); // Creates an atom and moves it from the parent's position
+        // This function is a test and will create a unit cell manually
 
+        /*Atoms = [atom1, atom2, atom3, atom4, atom5, atom6, atom7, atom8, atom9, atom10, atom11, atom12];
+        var  atom1 = Instantiate(Atom, parent.transform, false); // Creates an atom at the parent's position
+        var  atom2 = Instantiate(Atom, parent.transform, false);  atom2.transform.Translate(1f    * scale, 0f    * scale, 0f    * scale); // Creates an atom and moves it from the parent's position
+        var  atom3 = Instantiate(Atom, parent.transform, false);  atom3.transform.Translate(0f    * scale, 1f    * scale, 0f    * scale); // Creates an atom and moves it from the parent's position
+        var  atom4 = Instantiate(Atom, parent.transform, false);  atom4.transform.Translate(0f    * scale, 0f    * scale, 1f    * scale); // Creates an atom and moves it from the parent's position
+        var  atom5 = Instantiate(Atom, parent.transform, false);  atom5.transform.Translate(0f    * scale, 1f    * scale, 1f    * scale); // Creates an atom and moves it from the parent's position
+        var  atom6 = Instantiate(Atom, parent.transform, false);  atom6.transform.Translate(1f    * scale, 0f    * scale, 1f    * scale); // Creates an atom and moves it from the parent's position
+        var  atom7 = Instantiate(Atom, parent.transform, false);  atom7.transform.Translate(1f    * scale, 1f    * scale, 0f    * scale); // Creates an atom and moves it from the parent's position
+        var  atom8 = Instantiate(Atom, parent.transform, false);  atom8.transform.Translate(1f    * scale, 1f    * scale, 1f    * scale); // Creates an atom and moves it from the parent's position
+        var  atom9 = Instantiate(Atom, parent.transform, false);  atom9.transform.Translate(0.25f * scale, 0.25f * scale, 0.25f * scale); // Creates an atom and moves it from the parent's position
+        var atom10 = Instantiate(Atom, parent.transform, false); atom10.transform.Translate(0.25f * scale, 0.75f * scale, 0.75f * scale); // Creates an atom and moves it from the parent's position
+        var atom11 = Instantiate(Atom, parent.transform, false); atom11.transform.Translate(0.75f * scale, 0.25f * scale, 0.75f * scale); // Creates an atom and moves it from the parent's position
+        var atom12 = Instantiate(Atom, parent.transform, false); atom12.transform.Translate(0.75f * scale, 0.75f * scale, 0.25f * scale); // Creates an atom and moves it from the parent's position
+        */
     }
 
     // Read is called in Start
