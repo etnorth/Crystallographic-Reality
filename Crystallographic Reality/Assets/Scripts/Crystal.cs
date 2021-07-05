0ef45775274581cbe00bfb62357ce575d100705e
CyberMancerGamer <erlend.north@hotmail.com>
2021-07-05 16:57:19 +0200

    Conversion .cif->.xyz

--- Crystallographic Reality/Assets/Scripts/Crystal.cs
+++ Crystallographic Reality/Assets/Scripts/Crystal.cs
@@ -2,33 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System; // For StringSplitOptions to split e.g. 3 or 4 consecutive whitespaces (tab or one whitespace did not work) (also handles our Globalization)
 using System.IO; // IO: InputOutput. Used to read our input file
+//using System.Diagnostics; // Provides access to local and remote processes and enables you to start and stop local system processes
 
 public class Crystal : MonoBehaviour
 {
+    private string infile; //.cif-file filename and location
+    private int NumOfAtoms; // Total number of atoms
+    private string fileinfo; // cif2cell's info about converted file
+    private string[] atomElement; // The element of each atom
+    private Vector3[] atomPos; // The position of each atom
     public GameObject parent; // An object to be used as the parent of the unit cell
     public GameObject Atom; // Atom prefab
-    public int NumOfAtoms; // Total number of atoms
-    public GameObject[] Atoms; // Array of atom objects. Initialized in Start
-    public float scaleChange = 0.1f; // A scale for making unit cell smaller/larger.
+    private GameObject[] Atoms; // Array of atom objects. Initialized in Start
+    public double scaleChange = 0.1f; // A scale for making unit cell smaller/larger.
+    
+    
 
     // Start is called before the first frame update
     void Start()
     {
         // Initialization
-        Read_cif();
+        infile = @"C:\Users\erlen\Documents\Github\Crystallographic-Reality\files\Si.cif"; // This will likely be input from user somehow. @ makes backslash parsable
 
+        Debug.Log("Testing comma: 5,4");
+        try
+        {
+            float.Parse("5,4");
+        }
+        catch (FormatException error)
+        {
+            Debug.Log("FormatException: We hate comma and refuse to understand it");
+        }
+
+
+        Debug.Log("Testing period: 5.4");
+        try
+        {
+            float.Parse("5.4", System.Globalization.CultureInfo.InvariantCulture);
+        }
+        catch (FormatException error)
+        {
+            Debug.Log("FormatException: We hate period and refuse to understand it");
+        }
+
+
+
+        Convert_cif(infile); // Converts .cif-file to .xyz-file using cif2cell
+        Read(Path.GetDirectoryName(infile) + @"\cif2cell_convert\" + Path.GetFileNameWithoutExtension(infile) + ".xyz"); // Reads converted .xyz-file (Could've had Convert_cif return file path to have this cleaner)
+
+
+
+        NumOfAtoms = 12; // TEMPORARY
         Atoms = new GameObject[NumOfAtoms]; // Initializes list of atoms
         for (int i = 0; i < NumOfAtoms; i++) // Loops over each atom
         {
             Atoms[i] = Instantiate(Atom, parent.transform, false); // Instantiate (Spawn) atom i
-            Atoms[i].transform.Translate(i, 0, 0); // Move atom i by x-direction. This will correspond to atom positions after read_cif is done
+            Atoms[i].transform.Translate(i/2f, 0, 0); // Move atom i by x-direction. This will correspond to atom positions after read_cif is done
             //Atoms[i].transform.localScale += scalechange;
         }
-
-
-        TestCreateCell();
-        //Read();
     }
 
     // Update is called once per frame
@@ -36,6 +69,7 @@ public class Crystal : MonoBehaviour
     {
         
         //Scale unit cell down
+        // USES OLD INPUT SYSTEM. DOES NOT WORK.
         /*
         if (trigger)
         {
@@ -52,11 +86,124 @@ public class Crystal : MonoBehaviour
         */
     }
 
-    // Read_cif is called in Start
+    // Called in Start
+    void Convert_cif(string infile)
+    {
+        // Converts a .cif-file to .xyz using python and cif2cell in the command line
+
+        System.Diagnostics.Process process = new System.Diagnostics.Process(); // Creates a process to run the Command Prompt
+        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo(); // Defines a variable to insert our information in
+        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden; // Hides the Command prompt window from user
+        startInfo.FileName = "cmd.exe"; // Calls for the command prompt
+        startInfo.Arguments = "/C cd " + Path.GetDirectoryName(infile) + " & python cif2cell " + Path.GetFileName(infile) + " -p xyz --no-reduce -o cif2cell_convert/" + Path.GetFileNameWithoutExtension(infile) + ".xyz"; // Moves to appropriate directory and calls for convertion of chosen .cif-file
+        process.StartInfo = startInfo; // Puts the information we have defined inside the process
+        process.Start(); // Starts the process
+
+        if (File.Exists(Path.GetDirectoryName(infile) + @"\\cif2cell_convert\\" + Path.GetFileNameWithoutExtension(infile) + ".xyz")) { // If the converted file exists in the correct location
+            Debug.Log(".cif-file converted to .xyz!"); // Prints to the Unity Console
+        }
+        else // Notifies that the program could not find the converted file in time (the if-process seems to sometimes be quicker than the conversion process, so could have false negatives)
+        {
+            Debug.Log("Cannot confirm that the conversion worked."); // Prints to the Unity Console
+        }
+    }
+
+    // Called in start
+    void Read(string cellfile)
+    {
+        // This function will read an .xyz-file and store the data to create the cell
+        if (!File.Exists(cellfile)) // If the file does not exist
+        {
+            // Notify that file does not exist
+            Debug.Log("Could not find the converted .xyz-file. This could get funky."); // Prints to the Unity Console
+        }
+
+
+        // Open the file to read from
+        using (StreamReader filestream = File.OpenText(cellfile)) // Declares filestream as our "StreamRead" which reads the input file. "using" handles closing and resources, like with open in python (I believe)
+        {
+            NumOfAtoms = int.Parse(filestream.ReadLine()); // Reads 1st line and converts to int (1st line is number of atoms)
+            fileinfo = filestream.ReadLine(); // Reads 2nd line (which is fileinfo from cif2cell)
+
+            atomElement = new string[NumOfAtoms]; // Creates an empty string array of length equaling the number of atoms
+            atomPos = new Vector3[NumOfAtoms];
+            string line;
+
+            int i = 0;
+            while ((line = filestream.ReadLine()) != null) // While filestream is not empty. Think I could have done this with for-loop and without FileStream
+            {
+                string[] words = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries); // Splits the line on each tab into words
+                atomElement[i] = words[0];
+                atomPos[i] = new Vector3(float.Parse(words[1], System.Globalization.CultureInfo.InvariantCulture),
+                    float.Parse(words[2], System.Globalization.CultureInfo.InvariantCulture),
+                    float.Parse(words[3], System.Globalization.CultureInfo.InvariantCulture)); // NullReferenceException: Object reference not set to an instance of an object
+            }
+        }
+
+
+        /*
+        string[] atomElement = new string[NumOfAtoms]; // Creates an empty string array of length equaling the number of atoms
+        Transform[] atomPos = new Transform[NumOfAtoms];
+
+
+        string[] lines = File.ReadAllLines(cellfile); // Reads all lines and saves it to an array of lines
+        
+        int i = 0;
+
+        foreach (string line in lines)
+        {
+            string[] words = line.Split('\t'); // Splits the line on each tab into words
+            
+            foreach (string word in words)
+            {
+                Debug.Log(word);
+            }
+
+            atomElement[i] = words[0];
+            atomPos[i].position = new Vector3(float.Parse(words[1]), float.Parse(words[2]), float.Parse(words[3]));
+
+            i++;
+        }
+        */
+    }
+
+    // Deprecated
     void Read_cif()
     {
-        //Reads a cif-file given by the user
+        //Reads a cif-file given by the user. Deprecated as we will read a different file type, but leaving it in should it be useful later in development.
+        double a_length;
+        double b_length;
+        double c_length;
+        double a_angle;
+        double b_angle;
+        double c_angle;
 
+        infile = @"C:\Users\erlen\Documents\Github\Crystallographic-Reality\cif files for converting\Si.cif";
+
+        if (!File.Exists(infile))
+        {
+            Debug.Log("File does not exist!");
+            return;
+        }
+
+        string[] lines = File.ReadAllLines(infile);
+
+        foreach (string line in lines)
+        {
+            if (line.Contains("_cell_length"))
+            {
+                string[] words = line.Split(' ');
+                Debug.Log("Word: " + words[1] + "\n");
+                if (words[1].Contains("("))
+                {
+                    words[1] = words[1].Remove(words[1].Length-3);
+                    Debug.Log("() removed: " + words[1] + "\n");
+                }
+                //a_length = double.Parse(words[1]);
+                a_length = double.Parse("5.43053");
+                Debug.Log("a: " + a_length);
+            }
+        }
     }
 
     // TestCreateCell is called in Start
@@ -81,24 +228,4 @@ public class Crystal : MonoBehaviour
     }
 
     // Read is called in Start
-    void Read()
-    {
-        // This function will read an input-file and store any useful data
-        string path = @"C:\Users\erlen\Documents\Github\Crystallographic-Reality\cif files for converting\Si.cif"; // Declaration of out input file
-        if (!File.Exists(path)) // If the file does not exist
-        {
-            // Notify that file does not exist
-            Debug.Log("File does not exist."); // Prints to the Unity Console
-        }
-
-        // Open the file to read from
-        using (StreamReader sr = File.OpenText(path)) // Declares sr as our "StreamRead" which reads the input file
-        {
-            string s;
-            while ((s = sr.ReadLine()) != null) // While sr is not empty
-            {
-                Debug.Log(s); // Prints to the Unity Console
-            }
-        }
-    }
 }
