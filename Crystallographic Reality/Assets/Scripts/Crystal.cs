6aa411cbdae38606a9a386802e67ca644db65d1c
CyberMancerGamer <erlend.north@hotmail.com>
2021-07-06 19:04:53 +0200

    .xyz-file does not give cell length as expected

--- Crystallographic Reality/Assets/Scripts/Crystal.cs
+++ Crystallographic Reality/Assets/Scripts/Crystal.cs
@@ -13,6 +13,9 @@ public class Crystal : MonoBehaviour
     private string fileinfo; // cif2cell's info about converted file
     private string[] atomElement; // The element of each atom
     private Vector3[] atomPos; // The position of each atom
+    private Vector3 cellLength; // Length of the sides of the cell
+
+
     public GameObject parent; // An object to be used as the parent of the unit cell
     public GameObject Atom; // Atom prefab
     private GameObject[] AtomObjects; // Array of atom objects.
@@ -125,10 +128,12 @@ public class Crystal : MonoBehaviour
                 atomPos[i] = new Vector3(float.Parse(words[1], System.Globalization.CultureInfo.InvariantCulture),
                     float.Parse(words[2], System.Globalization.CultureInfo.InvariantCulture),
                     float.Parse(words[3], System.Globalization.CultureInfo.InvariantCulture)); // NullReferenceException: Object reference not set to an instance of an object
+                
+                cellLength = Vector3.Max(atomPos[i], cellLength); // Compares positions to find lattice cell length. NOTE! This doesnt' necessarily give the cell length. Based on atoms not on cell.
+
                 i++;
             }
         }
-
 
         /*
         string[] atomElement = new string[NumOfAtoms]; // Creates an empty string array of length equaling the number of atoms
@@ -158,20 +163,73 @@ public class Crystal : MonoBehaviour
 
     void CreateCell()
     {
-        // Creates the cell that is being investigated.
+        // Constructs the cell that is being investigated.
+
+        /* Inverts celllength and normalizes positions. Works, but unless we shrink the atoms, they overlap and when scaling crystal, they become way too large.
+        for (int i = 0; i < 3; i++)
+        {
+            cellLength[i] = 1.0f / cellLength[i]; // Inverts cellLength so we can normalize each atomPos with Vector3.Scale
+        }
+
+        for (int i = 0; i < NumOfAtoms; i++)
+        {
+            atomPos[i] = Vector3.Scale(atomPos[i], cellLength); // Normalizes atomPos so everything is fractional in the crystal
+        }
+        */
+
+        foreach (Vector3 position in atomPos) // Iterates over each atomPos
+        {
+            for (int i = 0; i < 3; i++) // Iterates over each dimension (x, y, z)
+            {
+                if (position[i] == 0f)
+                {
+
+                }
+            }
+
+        }
+
 
         AtomObjects = new GameObject[NumOfAtoms]; // Initializes the array of AtomObjects with a set length so we can fill the array iteratively
+        Vector3 tempPosition; // Initializes position for equivalent atoms (atoms on edges or corners will be duplicated in other edges and corners)
 
         for (int i = 0; i<NumOfAtoms; i++)
         {
-            //AtomObjects[i] = Instantiate(Atom, atomPos[i], new Quaternion(), parent.transform);
             AtomObjects[i] = Instantiate(Atom, parent.transform, false); // Creates an atom with its position relative to the parent
             AtomObjects[i].transform.Translate(atomPos[i]); // Sets the atom position to match that of the .xyz-file (can do scaling in Update() )
+
+            // Creates atoms not in the .xyz-file, but still useful or necessary
+            tempPosition = atomPos[i];
+
+            cellLength = new Vector3(5.43053f, 5.43053f, 5.43053f); // VERY TEMPORARY FIX
+
+            for (int j = 0; j < 3; j++) // Iterates over x, y and z
+            {
+                if (Mathf.Abs(atomPos[i][j]) < 0.0001f) // If atom position is approx. 0
+                {
+                    tempPosition[j] = cellLength[j]; // Sets start of cell to end of cell to duplicate other end of cell for equivalent atom
+                    Instantiate(Atom, parent.transform, false).transform.Translate(tempPosition); // Creates equivalent atom AND sets the position
+                }
+                /* This is actually ok without
+                else if (Mathf.Abs(cellLength[j] - atomPos[i][j]) < 0.0001f) // If atom position is approx. cellLength
+                {
+                    tempPosition[j] = 0f; // Sets end of cell to start of cell to duplicate other end of cell for equivalent atom
+                    Instantiate(Atom, parent.transform, false).transform.Translate(tempPosition); // Creates equivalent atom AND sets the position
+                }*/
+            }
         }
 
+        // Takes care of the leftover corner atoms ( (0,0,0), (1,0,0), (1,1,0) and (1,1,1) taken care of above)
+        if (atomPos[0][0] < 0.0001f && atomPos[0][1] < 0.0001f && atomPos[0][2] < 0.0001f) // If there is an atom in origo (as a corner)
+        {
+            Instantiate(Atom, parent.transform, false).transform.Translate(cellLength[0], atomPos[0][1], cellLength[2]); // 1,0,1
+            Instantiate(Atom, parent.transform, false).transform.Translate(atomPos[0][0], cellLength[1], atomPos[0][2]); // 0,1,0
+            Instantiate(Atom, parent.transform, false).transform.Translate(atomPos[0][0], cellLength[1], cellLength[2]); // 0,1,1
+            Instantiate(Atom, parent.transform, false).transform.Translate(atomPos[0][0], atomPos[0][1], cellLength[2]); // 0,0,1
+        }
     }
 
-    // Deprecated
+    /* Deprecated
     void Read_cif()
     {
         //Reads a cif-file given by the user. Deprecated as we will read a different file type, but leaving it in should it be useful later in development.
@@ -209,13 +267,14 @@ public class Crystal : MonoBehaviour
             }
         }
     }
+    */
 
-    // TestCreateCell is called in Start
+    /* TestCreateCell is called in Start
     void TestCreateCell()
     {
         // This function is a test and will create a unit cell manually
 
-        /*Atoms = [atom1, atom2, atom3, atom4, atom5, atom6, atom7, atom8, atom9, atom10, atom11, atom12];
+        Atoms = [atom1, atom2, atom3, atom4, atom5, atom6, atom7, atom8, atom9, atom10, atom11, atom12];
         var  atom1 = Instantiate(Atom, parent.transform, false); // Creates an atom at the parent's position
         var  atom2 = Instantiate(Atom, parent.transform, false);  atom2.transform.Translate(1f    * scale, 0f    * scale, 0f    * scale); // Creates an atom and moves it from the parent's position
         var  atom3 = Instantiate(Atom, parent.transform, false);  atom3.transform.Translate(0f    * scale, 1f    * scale, 0f    * scale); // Creates an atom and moves it from the parent's position
@@ -228,8 +287,7 @@ public class Crystal : MonoBehaviour
         var atom10 = Instantiate(Atom, parent.transform, false); atom10.transform.Translate(0.25f * scale, 0.75f * scale, 0.75f * scale); // Creates an atom and moves it from the parent's position
         var atom11 = Instantiate(Atom, parent.transform, false); atom11.transform.Translate(0.75f * scale, 0.25f * scale, 0.75f * scale); // Creates an atom and moves it from the parent's position
         var atom12 = Instantiate(Atom, parent.transform, false); atom12.transform.Translate(0.75f * scale, 0.75f * scale, 0.25f * scale); // Creates an atom and moves it from the parent's position
-        */
+        
     }
-
-    // Read is called in Start
+    */
 }
