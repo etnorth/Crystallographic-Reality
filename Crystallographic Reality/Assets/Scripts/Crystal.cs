a31e6a0d0e29f596aec37fddb8e585087efd867b
CyberMancerGamer <erlend.north@hotmail.com>
2021-07-15 21:36:02 +0200

    Fixed cell generation for corners, sides, and edges

--- Crystallographic Reality/Assets/Scripts/Crystal.cs
+++ Crystallographic Reality/Assets/Scripts/Crystal.cs
@@ -9,16 +9,17 @@ using System.IO; // IO: InputOutput. Used to read our input file
 public class Crystal : MonoBehaviour
 {
     private string infile; //.cif-file filename and location
-    private int NumOfAtoms; // Total number of atoms
+    private int NumOfAtoms; // Number of UNIQUE atoms in the conventional cell (aka. number of atoms from input file, not the amount of atoms that are created by the end)
     private string fileinfo; // cif2cell's info about converted file
     private string[] atomElement; // The element of each atom
     private Vector3[] atomPos; // The position of each atom
     private Vector3 cellLength; // Length of the sides of the cell
+    private Vector3 cellAngle; // Angle of the lattice vectors
 
 
-    public GameObject parent; // An object to be used as the parent of the unit cell
-    public GameObject Atom; // Atom prefab
-    private GameObject[] AtomObjects; // Array of atom objects.
+    public GameObject parent; // An object to be used as the parent of the unit cell. In Unity I have selected an empty parent object "Crystal" for this.
+    public GameObject atom; // Atom prefab. Selected manually in Unity
+    private GameObject[] atomObjects; // Array of atom objects.
     public double scaleChange = 0.1f; // A scale for making unit cell smaller/larger.
     
     
@@ -34,7 +35,7 @@ public class Crystal : MonoBehaviour
         {
             ConvertCifToXyz(infile); // Converts .cif-file to .xyz-file using cif2cell (uses --no-reduce to get the conventional cell and not the primitive cell. This could maybe be changed by the user later)
         }
-        Read(Path.GetDirectoryName(infile) + @"\cif2cell_convert\" + Path.GetFileNameWithoutExtension(infile) + ".xyz"); // Reads converted .xyz-file (Could've had Convert_cif return file path to have this cleaner)
+        ReadXYZ(Path.GetDirectoryName(infile) + @"\cif2cell_convert\" + Path.GetFileNameWithoutExtension(infile) + ".xyz"); // Reads converted .xyz-file (Could've had Convert_cif return file path to have this cleaner)
         CreateCell();
 
 
@@ -43,8 +44,8 @@ public class Crystal : MonoBehaviour
         AtomObjects = new GameObject[NumOfAtoms]; // Initializes list of atoms
         for (int i = 0; i < NumOfAtoms; i++) // Loops over each atom
         {
-            AtomObjects[i] = Instantiate(Atom, parent.transform, false); // Instantiate (Spawn) atom i
-            AtomObjects[i].transform.Translate(i/2f, 0, 0); // Move atom i by x-direction. This will correspond to atom positions after read_cif is done
+            atomObjects[i] = Instantiate(Atom, parent.transform, false); // Instantiate (Spawn) atom i
+            atomObjects[i].transform.Translate(i/2f, 0, 0); // Move atom i by x-direction. This will correspond to atom positions after read_cif is done
             //Atoms[i].transform.localScale += scalechange;
         }
         */
@@ -85,7 +86,6 @@ public class Crystal : MonoBehaviour
             " & python cif2cell " + Path.GetFileName(infile) + 
             " --program=xyz --no-reduce --cartesian --outputfile=cif2cell_convert/" + 
             Path.GetFileNameWithoutExtension(infile) + ".xyz"; // Moves to appropriate directory and calls for convertion of chosen .cif-file
-        //startInfo.Arguments = "/C cd " + Path.GetDirectoryName(infile) + " & python cif2cell " + Path.GetFileName(infile) + " -p xyz --no-reduce --supercell=[2,2,2] -o cif2cell_convert/" + Path.GetFileNameWithoutExtension(infile) + ".xyz"; // Moves to appropriate directory and calls for convertion of chosen .cif-file (SHOULD BE A SUPERCELL)
         process.StartInfo = startInfo; // Puts the information we have defined inside the process
         process.Start(); // Starts the process
         process.WaitForExit(); // Waits for the process to end before continuing
@@ -93,14 +93,51 @@ public class Crystal : MonoBehaviour
         if (File.Exists(Path.GetDirectoryName(infile) + @"\\cif2cell_convert\\" + Path.GetFileNameWithoutExtension(infile) + ".xyz")) { // If the converted file exists in the correct location
             Debug.Log(".cif-file converted to .xyz!"); // Prints to the Unity Console
         }
-        else // Notifies that the program could not find the converted file in time
+        else // Notifies that the program could not find the converted file
         {
             Debug.Log("Cannot confirm that the conversion worked."); // Prints to the Unity Console
         }
+
+        // .xyz does not include the lattice vectors, so we fetch those manually from the .cif-file
+
+        Debug.Log("Fetching cell parameters manually"); // Prints to the Unity Console
+        string[] lines = File.ReadAllLines(infile); // Reads the .cif file as an array of lines
+        int i = 0; // Counter for cellLength
+        int j = 0; // Counter for cellAngle
+        foreach (string line in lines)
+        {
+            if (line.Contains("_cell_length")) // Looks for the length of the sides of the cell
+            {
+                string[] words = line.Split(' '); // Splits the line into an array of words. Splits by whitespace
+                if (words[1].Contains("(")) // If the file has included uncertainty, remove it.
+                {
+                    words[1] = words[1].Remove(words[1].Length - 3);
+                }
+                cellLength[i] = float.Parse(words[1], System.Globalization.CultureInfo.InvariantCulture); // Sets index i to the length (.cif uses x->y->z so 0->1->2 should be fine)
+                
+                i++;
+            }
+            else if (line.Contains("_cell_angle"))
+            {
+                string[] words = line.Split(' '); // Splits the line into an array of words. Splits by whitespace
+                if (words[1].Contains("(")) // If the file has included uncertainty, remove it. Likely not the case for angles.
+                {
+                    words[1] = words[1].Remove(words[1].Length - 3);
+                }
+                cellAngle[j] = float.Parse(words[1], System.Globalization.CultureInfo.InvariantCulture); // Sets index i to the angle (.cif uses x->y->z so 0->1->2 should be fine)
+                
+                j++;
+            }
+            else if (i == 2 && j == 2) // When we have gotten all lengths and angles, stop the loop (saves time)
+            {
+                break;
+            }
+        }
+
     }
 
-    // Called in start
-    void Read(string cellfile)
+    // Called in Start
+    void ReadXYZ(string cellfile)
     {
         // This function will read an .xyz-file and store the data to create the cell
         if (!File.Exists(cellfile)) // If the file does not exist
@@ -108,7 +145,6 @@ public class Crystal : MonoBehaviour
             // Notify that file does not exist
             Debug.Log("Could not find the converted .xyz-file. This could get funky."); // Prints to the Unity Console
         }
-
 
         // Open the file to read from
         using (StreamReader filestream = File.OpenText(cellfile)) // Declares filestream as our "StreamRead" which reads the input file. "using" handles closing and resources, like with open in python (I believe)
@@ -128,8 +164,6 @@ public class Crystal : MonoBehaviour
                 atomPos[i] = new Vector3(float.Parse(words[1], System.Globalization.CultureInfo.InvariantCulture),
                     float.Parse(words[2], System.Globalization.CultureInfo.InvariantCulture),
                     float.Parse(words[3], System.Globalization.CultureInfo.InvariantCulture)); // NullReferenceException: Object reference not set to an instance of an object
-                
-                cellLength = Vector3.Max(atomPos[i], cellLength); // Compares positions to find lattice cell length. NOTE! This doesnt' necessarily give the cell length. Based on atoms not on cell.
 
                 i++;
             }
@@ -161,6 +195,7 @@ public class Crystal : MonoBehaviour
         */
     }
 
+    // Called in Start
     void CreateCell()
     {
         // Constructs the cell that is being investigated.
@@ -177,56 +212,95 @@ public class Crystal : MonoBehaviour
         }
         */
 
-        foreach (Vector3 position in atomPos) // Iterates over each atomPos
+        //atomObjects = new GameObject[NumOfAtoms]; // Initializes the array of AtomObjects with a set length so we can fill the array iteratively
+        List<GameObject> atomObjectsList = new List<GameObject>();
+        Vector3 equivalentPosition; // Initializes position for equivalent atoms (atoms on edges or corners will be duplicated in other edges and corners)
+        List<GameObject> equivalentAtomObjects = new List<GameObject>(); // Creates a list for the new atoms
+        int equivalentCount = 0;
+
+        
+        for (int i = 0; i < NumOfAtoms; i++)
         {
-            for (int i = 0; i < 3; i++) // Iterates over each dimension (x, y, z)
-            {
-                if (position[i] == 0f)
-                {
-
-                }
-            }
-
-        }
-
-
-        AtomObjects = new GameObject[NumOfAtoms]; // Initializes the array of AtomObjects with a set length so we can fill the array iteratively
-        Vector3 tempPosition; // Initializes position for equivalent atoms (atoms on edges or corners will be duplicated in other edges and corners)
-
-        for (int i = 0; i<NumOfAtoms; i++)
-        {
-            AtomObjects[i] = Instantiate(Atom, parent.transform, false); // Creates an atom with its position relative to the parent
-            AtomObjects[i].transform.Translate(atomPos[i]); // Sets the atom position to match that of the .xyz-file (can do scaling in Update() )
+            atomObjectsList.Add(Instantiate(atom, parent.transform, false)); // Creates an atom with its position relative to the parent
+            atomObjectsList[i].transform.Translate(atomPos[i]); // Sets the atom position to match that of the .xyz-file (can do scaling in Update() )
 
             // Creates atoms not in the .xyz-file, but still useful or necessary
-            tempPosition = atomPos[i];
-
-            cellLength = new Vector3(5.43053f, 5.43053f, 5.43053f); // VERY TEMPORARY FIX
-
             for (int j = 0; j < 3; j++) // Iterates over x, y and z
             {
                 if (Mathf.Abs(atomPos[i][j]) < 0.0001f) // If atom position is approx. 0
                 {
-                    tempPosition[j] = cellLength[j]; // Sets start of cell to end of cell to duplicate other end of cell for equivalent atom
-                    Instantiate(Atom, parent.transform, false).transform.Translate(tempPosition); // Creates equivalent atom AND sets the position
+                    equivalentPosition = atomPos[i];
+
+                    equivalentPosition[j] = cellLength[j]; // Sets start of cell to end of cell to duplicate other end of cell for equivalent atom
+                    equivalentAtomObjects.Add(Instantiate(atom, parent.transform, false)); // Instantiates an equivalent atom to the original
+                    equivalentAtomObjects[equivalentCount].transform.Translate(equivalentPosition); // Sets the equivalent position
+                    Debug.Log("1st created " + equivalentPosition + " from " + atomPos[i]);
+                    equivalentCount++;
                 }
-                /* This is actually ok without
-                else if (Mathf.Abs(cellLength[j] - atomPos[i][j]) < 0.0001f) // If atom position is approx. cellLength
-                {
-                    tempPosition[j] = 0f; // Sets end of cell to start of cell to duplicate other end of cell for equivalent atom
-                    Instantiate(Atom, parent.transform, false).transform.Translate(tempPosition); // Creates equivalent atom AND sets the position
-                }*/
             }
         }
 
-        // Takes care of the leftover corner atoms ( (0,0,0), (1,0,0), (1,1,0) and (1,1,1) taken care of above)
-        if (atomPos[0][0] < 0.0001f && atomPos[0][1] < 0.0001f && atomPos[0][2] < 0.0001f) // If there is an atom in origo (as a corner)
+        
+        // Solves the cases where two coordinates are zero. NOTE: This creates duplicates of atoms as (x,1,0) and (x,0,1) from previous loop are flipped to (x,1,1). Will destroy duplicates.
+        List<GameObject> moreEquivalentAtomObjects = new List<GameObject>();
+        equivalentCount = 0;
+
+        for (int i = 0; i < equivalentAtomObjects.Count; i++)
         {
-            Instantiate(Atom, parent.transform, false).transform.Translate(cellLength[0], atomPos[0][1], cellLength[2]); // 1,0,1
-            Instantiate(Atom, parent.transform, false).transform.Translate(atomPos[0][0], cellLength[1], atomPos[0][2]); // 0,1,0
-            Instantiate(Atom, parent.transform, false).transform.Translate(atomPos[0][0], cellLength[1], cellLength[2]); // 0,1,1
-            Instantiate(Atom, parent.transform, false).transform.Translate(atomPos[0][0], atomPos[0][1], cellLength[2]); // 0,0,1
+
+            for (int j = 1; j < 3; j++) // Iterates over y and z
+            {
+                if (Mathf.Abs(equivalentAtomObjects[i].transform.localPosition[j]) < 0.0001f) // If atom position is approx. 0
+                {
+                    equivalentPosition = equivalentAtomObjects[i].transform.localPosition;
+
+                    equivalentPosition[j] = cellLength[j]; // Sets start of cell to end of cell to duplicate other end of cell for equivalent atom
+                    moreEquivalentAtomObjects.Add(Instantiate(atom, parent.transform, false)); // Instantiates an equivalent atom to the original
+                    moreEquivalentAtomObjects[equivalentCount].transform.Translate(equivalentPosition); // Sets the equivalent position
+                    Debug.Log("2nd created " + equivalentPosition + " from " + equivalentAtomObjects[i].transform.localPosition);
+                    equivalentCount++;
+                }
+            }
         }
+
+        // Solves the case where three coordinates are zero (origin)
+        List<GameObject> evenMoreEquivalentAtomObjects = new List<GameObject>();
+        equivalentCount = 0;
+
+        for (int i = 0; i < moreEquivalentAtomObjects.Count; i++)
+        {
+
+            // Iterates over z
+            if (Mathf.Abs(moreEquivalentAtomObjects[i].transform.localPosition[2]) < 0.0001f) // If atom position is approx. 0
+            {
+                equivalentPosition = moreEquivalentAtomObjects[i].transform.localPosition;
+
+                equivalentPosition[2] = cellLength[2]; // Sets start of cell to end of cell to duplicate other end of cell for equivalent atom
+                evenMoreEquivalentAtomObjects.Add(Instantiate(atom, parent.transform, false)); // Instantiates an equivalent atom to the original
+                evenMoreEquivalentAtomObjects[equivalentCount].transform.Translate(equivalentPosition); // Sets the equivalent position
+                Debug.Log("3rd created " + equivalentPosition + " from " + moreEquivalentAtomObjects[i].transform.localPosition);
+                equivalentCount++;
+            }
+
+        }
+
+        // Adds lists together
+        atomObjectsList.AddRange(equivalentAtomObjects);
+        atomObjectsList.AddRange(moreEquivalentAtomObjects);
+        atomObjectsList.AddRange(evenMoreEquivalentAtomObjects);
+
+
+        // Destroys duplicate atoms
+        for (int i = 0; i < atomObjectsList.Count-1; i++)
+        {
+            if (atomObjectsList[i].transform.position == atomObjectsList[i+1].transform.position) // If position vectors are equal (Vector3 includes approximation)
+            {
+                Destroy(atomObjectsList[i + 1]);
+            }
+        }
+
+
+        atomObjects = atomObjectsList.ToArray(); // Converts the atomObjectsList to an array (arrays are better, faster, harder, stronger)
     }
 
     /* Deprecated
