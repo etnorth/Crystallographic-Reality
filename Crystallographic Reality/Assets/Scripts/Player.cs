6636e3691fedf758f4a72072c895f90c49b9250c
CyberMancerGamer <erlend.north@hotmail.com>
2021-08-24 12:07:49 +0200

    Re-bound player scale and removed UI in mainScene

--- Crystallographic Reality/Assets/Scripts/Player.cs
+++ Crystallographic Reality/Assets/Scripts/Player.cs
@@ -10,11 +10,19 @@ public class Player : MonoBehaviour
     public InputActionReference scaleUpReference;
     public InputActionReference scaleDownReference;
 
+    //GameObject symmetryObject = Crystal.crystal.transform.Find("Symmetries"); // Needed an object reference (script needs to be sure that the object exists)
+    private GameObject crystal; // Could also use public and put the crystal object on the script, or use Serialize to keep it private, but accessable in the editor. This avoids .Find()
+    private GameObject symmetries;
+    public InputActionReference symmetryReference;
+
+    private void Start()
+    {
+        crystal = GameObject.Find("Crystal"); // Finds a GameObject named "Crystal"
+    }
+
     private void Update()
     {
-        //bool up = scaleUpReference.action.ReadValue<bool>();
-        //bool down = scaleDownReference.action.ReadValue<bool>();
-
+        // Player scaling
         if (scaleUpReference.action.triggered)
         {
             gameObject.transform.localScale *= 1.1f;
@@ -23,10 +31,22 @@ public class Player : MonoBehaviour
         {
             gameObject.transform.localScale *= 0.9f;
         }
-    }
 
-    private void Scale(InputAction.CallbackContext context)
-    {
+        // Show/hide symmetry
+        if (symmetryReference.action.triggered) // If button is pressed, ON->OFF & OFF->ON
+        {
+            symmetries = crystal.transform.Find("Symmetries").gameObject; // Finds a child of the crystal called "Symmetries" and gets the object
+            if (symmetries.activeSelf)
+            {
+                symmetries.SetActive(false);
+            }
+            else
+            {
+                symmetries.SetActive(true);
+            }
+            
+        }
+
 
     }
 }
