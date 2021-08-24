d2cea0726338c4ae5aae9f4035af401beba58b19
CyberMancerGamer <erlend.north@hotmail.com>
2021-08-24 17:08:19 +0200

    Added a Runtime File Browser
    and began figuring out how it works

--- Crystallographic Reality/Assets/Scripts/ButtonHandler.cs
+++ Crystallographic Reality/Assets/Scripts/ButtonHandler.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEditor; // For File Dialog
+//using UnityEditor; // For File Dialog
+using SimpleFileBrowser;
 using UnityEngine.UI; // For getting Text Component
 using System; // For exceptions
 using System.IO; // For GetFileName (aka. removing path)
@@ -10,6 +11,7 @@ using UnityEngine.SceneManagement; // For changing scene
 
 public class ButtonHandler : MonoBehaviour
 {
+    /* Used UnityEditor, which is disabled for builds.
     public void GetInfile() // Public allows us to select it in the Unity Editor
     {
         string infile = EditorUtility.OpenFilePanel("Load your crystal file", "", "cif"); // Opens a file panel to find the .cif-file
@@ -23,6 +25,21 @@ public class ButtonHandler : MonoBehaviour
 
         Debug.Log("User clicked the File Button and selected " + infile);
     }
+    */
+    public void GetInfile() // Public allows us to select it in the Unity Editor
+    {
+        string infile = ("hey"); // Opens a file panel to find the .cif-file
+        if (!File.Exists(infile))
+        {
+            transform.Find("Text").GetComponent<Text>().text = "File not found"; // Updates Button Text
+            throw new FileNotFoundException("Could not find chosen .cif-file");
+        }
+        transform.Find("Text").GetComponent<Text>().text = Path.GetFileName(infile); // Updates the text of the button to match the selected file
+        CrystalManager.infile = infile; // Stores our infile in a static class so it can be fetched in another scene
+
+        Debug.Log("User clicked the File Button and selected " + infile);
+    }
+
     public void LaunchGame()
     {
         CrystalManager.TestSetup();
